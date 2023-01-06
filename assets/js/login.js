6aa600bd911634295632e02a2debbbe47479c2ce
Gustavo Waki <90512847+GustavoWakiBarbosa@users.noreply.github.com>
2023-01-06 19:08:52 -0300

    Parte estética da tela de login criada

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,11 +1,17 @@
-const professor = document.querySelector("#email")
+const professor = document.querySelector("#tipo-cadastro-prof")
+const aluno = document.querySelector("#tipo-cadastro-aluno")
 const areaLogin = document.querySelector(".area-login")
+const registro = document.querySelector(".registrar")
+const principal = document.querySelector(".principal")
 
-if(professor.checked){
-    areaLogin.style.cssText =
-    "display: none;"+
-    "flex-direction: column;"+
-    "justify-content: center;"+
-    "align-items: center;"+
-    "gap: 10px;"
-}
+professor.addEventListener("click", () => {
+    areaLogin.style.display = "flex";
+    registro.style.display = "block"
+    principal.style.backgroundColor = "var(--roxo-escuro)"
+})   
+
+aluno.addEventListener("click", () => {
+    areaLogin.style.display = "flex";
+    registro.style.display = "none"
+    principal.style.backgroundColor = "var(--rosa-escuro)"
+})   
