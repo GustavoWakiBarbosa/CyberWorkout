e9a5ad5f4ffc640d06e04c6b60de475651e84c6f
Gustavo Waki <90512847+GustavoWakiBarbosa@users.noreply.github.com>
2023-01-22 00:35:31 -0300

    sistema de pastas parte 1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,49 @@
 const button = document.querySelector('.button-pasta');
 const buttonF = document.querySelector('#button-cancelar')
+const buttonC = document.querySelector('#button-criar')
 const container = document.querySelector('.modal-container');
 const modal = document.querySelector('.modal');
+const listaPastas = document.querySelector('.lista-pastas');
 
 const activeModalClass = 'modal-show';
 
 const openModal = () => container.classList.add(activeModalClass);
 const closeModal = () => container.classList.remove(activeModalClass);
 
-button.addEventListener('click', () => {
+
+button.addEventListener('click', openModal);
+container.addEventListener('click', (event) => {
+    if (modal.contains(event.target)) return;
+    
+    closeModal();
+});
+
+button.addEventListener('click', (event) => {
     event.preventDefault()
     openModal()
   })
   
-  buttonF.addEventListener('click', () => {
+  buttonF.addEventListener('click', (event) => {
     event.preventDefault()
     closeModal()
   })
 
-  button.addEventListener('click', openModal);
-  container.addEventListener('click', (event) => {
-      if (modal.contains(event.target)) return;
-      
-      closeModal();
-  });
+  buttonC.addEventListener('click', (event) => {
+    event.preventDefault()
+    const nomePasta = document.querySelector("#nomePasta").value
+    let template = document.querySelector("template");
+    let itemNovo = template.content.cloneNode(true);
+    itemNovo = itemNovo.querySelector(".container-pasta")
+    // console.log(itemNovo.children[1])
+    itemNovo.children[1].textContent = nomePasta
+    console.log(itemNovo)
+
+    const main = document.querySelector("main")
+    main.appendChild(itemNovo)
+  })
+
+  // seta.addEventListener('click', () => {
+
+  // })
+
   
