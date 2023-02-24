6ca73f06f3a6b13ef647762b7137beec28a702b7
Gustavo Waki <90512847+GustavoWakiBarbosa@users.noreply.github.com>
2023-02-24 19:51:06 -0300

    criada função de criar séries

--- assets/js/treino/serie.js
+++ assets/js/treino/serie.js
@@ -1,8 +1,20 @@
 listaEx.addEventListener("click", function(event) {
     let alvoEvento = event.target;
     console.log(alvoEvento)
-    if (alvoEvento.classList.contains == 'add-serie') {
-      let numLinhasQuebradas = alvoEvento.value.split('\n').length;
-      alvoEvento.rows = numLinhasQuebradas;
+    if (alvoEvento.id == 'add-serie') {
+      paiDoAlvo = alvoEvento.parentNode
+      listaSeries = paiDoAlvo.querySelector('.lista-series')
+      const templateS = listaSeries.querySelector('.template-serie')
+      const serieNova = templateS.content.cloneNode(true)
+      listaSeries.appendChild(serieNova)
+    }
+
+    if(alvoEvento.classList.contains('check-ex')){
+      if(alvoEvento.classList.contains('feito')){
+        alvoEvento.classList.remove("feito")
+      }else{
+        alvoEvento.classList.add("feito")
+      }
+      
     }
   });
