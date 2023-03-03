143152fc051c2da163f076d152d3d7cb93525265
Gustavo Waki <90512847+GustavoWakiBarbosa@users.noreply.github.com>
2023-03-02 22:58:13 -0300

    feet: está sendo criada a funçâo de mudar o tipo de série do exercício
    Está com um bug em que altera o texto e cor de todas as séries, é necessário corrigir

--- assets/js/treino/serie.js
+++ assets/js/treino/serie.js
@@ -22,5 +22,40 @@ listaEx.addEventListener("click", function(event) {
         },500)
     }else if(alvoEvento.hasAttribute("contenteditable")){
       alvoEvento.textContent = ""
+    }else if(alvoEvento.classList.contains("serie")){
+      const modalTipo = document.querySelector('.modal-tipo')
+      const containerT = document.querySelector('.modal-container-tipo')
+
+      const openModalT = () => containerT.classList.add('modal-show')
+      const closeModalT = () => containerT.classList.remove('modal-show')
+
+      openModalT()
+      
+      const buttonCT = document.querySelector("#button-cancelar-serie")
+
+      buttonCT.addEventListener('click', (event) =>{
+        closeModalT()
+      })
+
+      containerT.addEventListener('click', (event) => {
+        if (modal.contains(event.target)) return;
+        
+        closeModalT();
+        })
+
+        modalTipo.addEventListener("click", function(event) {
+          console.log(alvoEvento)
+          alvoTipo = event.target
+          if(alvoTipo.classList.contains("warm-up")){
+            alvoEvento.textContent = "W"
+            alvoEvento.style.color = "#ffc124"
+          }else if(alvoTipo.classList.contains("normal")){
+            alvoEvento.textContent = "N"
+            alvoEvento.style.color = "#FFFFFF"
+          }else if(alvoTipo.classList.contains("failure")){
+            alvoEvento.textContent = "F"
+            alvoEvento.style.color = "#ff0808"
+          }
+        })
     }
   });
