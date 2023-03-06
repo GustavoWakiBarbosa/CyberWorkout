630cbe1158e6e56d73f2739e68fa562c1a1a3715
Gustavo Waki <90512847+GustavoWakiBarbosa@users.noreply.github.com>
2023-03-06 19:46:36 -0300

    test: possível correção de bug 2

--- assets/js/treino/serie.js
+++ assets/js/treino/serie.js
@@ -1,65 +1,65 @@
 listaEx.addEventListener("click", function(event) {
-    var alvoEvento = event.target;
-    if (alvoEvento.id == 'add-serie') {
-      paiDoAlvo = alvoEvento.parentNode
-      listaSeries = paiDoAlvo.querySelector('.lista-series')
-      const templateS = listaSeries.querySelector('.template-serie')
-      const serieNova = templateS.content.cloneNode(true)
-      listaSeries.appendChild(serieNova)
+  var alvoEvento = event.target
+    
+  if (alvoEvento.id == 'add-serie') {
+    paiDoAlvo = alvoEvento.parentNode
+    listaSeries = paiDoAlvo.querySelector('.lista-series')
+    const templateS = listaSeries.querySelector('.template-serie')
+    const serieNova = templateS.content.cloneNode(true)
+    listaSeries.appendChild(serieNova)
+  }
+
+  if(alvoEvento.classList.contains('check-ex')){
+    if(alvoEvento.classList.contains('feito')){
+      alvoEvento.classList.remove("feito")
+    }else{
+      alvoEvento.classList.add("feito")
     }
+  }else if(alvoEvento.classList.contains("pontinhos")){
+    let paiDoAlvo = alvoEvento.parentNode.parentNode
+      paiDoAlvo.classList.add("fadeOut")
+      setTimeout(function(){
+        paiDoAlvo.remove()
+      },500)
+  }else if(alvoEvento.hasAttribute("contenteditable")){
+    alvoEvento.textContent = ""
+  }else if(alvoEvento.classList.contains("serie")){
+    const modalTipo = document.querySelector('.modal-tipo')
+    const containerT = document.querySelector('.modal-container-tipo')
 
-    if(alvoEvento.classList.contains('check-ex')){
-      if(alvoEvento.classList.contains('feito')){
-        alvoEvento.classList.remove("feito")
-      }else{
-        alvoEvento.classList.add("feito")
-      }
-    }else if(alvoEvento.classList.contains("pontinhos")){
-      let paiDoAlvo = alvoEvento.parentNode.parentNode
-        paiDoAlvo.classList.add("fadeOut")
-        setTimeout(function(){
-          paiDoAlvo.remove()
-        },500)
-    }else if(alvoEvento.hasAttribute("contenteditable")){
-      alvoEvento.textContent = ""
-    }else if(alvoEvento.classList.contains("serie")){
-      const modalTipo = document.querySelector('.modal-tipo')
-      const containerT = document.querySelector('.modal-container-tipo')
+    const openModalT = () => containerT.classList.add('modal-show')
+    const closeModalT = () => containerT.classList.remove('modal-show')
 
-      const openModalT = () => containerT.classList.add('modal-show')
-      const closeModalT = () => containerT.classList.remove('modal-show')
+    openModalT()
+    
+    const buttonCT = document.querySelector("#button-cancelar-serie")
 
-      openModalT()
+    buttonCT.addEventListener('click', (event) =>{
+      closeModalT()
+    })
+
+    containerT.addEventListener('click', (event) => {
+      if (modal.contains(event.target)) return;
       
-      const buttonCT = document.querySelector("#button-cancelar-serie")
-
-      buttonCT.addEventListener('click', (event) =>{
-        closeModalT()
+      closeModalT();
       })
 
-      containerT.addEventListener('click', (event) => {
-        if (modal.contains(event.target)) return;
-        
-        closeModalT();
-        })
-
-      modalTipo.addEventListener("click", function(event2) {
-        console.log(alvoEvento)
-        alvoTipo = event2.target
-        if(alvoTipo.classList.contains("warm-up")){
-          alvoEvento.textContent = "W"
-          alvoEvento.style.color = "#ffc124"
-        }else if(alvoTipo.classList.contains("normal")){
-          alvoEvento.textContent = "N"
-          alvoEvento.style.color = "#FFFFFF"
-        }else if(alvoTipo.classList.contains("failure")){
-          alvoEvento.textContent = "F"
-          alvoEvento.style.color = "#ff0808"
-        }else if(alvoTipo.classList.contains("drop")){
-          alvoEvento.textContent = "D"
-          alvoEvento.style.color = "#0095ff"
-        }
-        alvoEvento = null
-      })
-    }
+    modalTipo.addEventListener("click", function(event2) {
+      alvoTipo = event2.target
+      if(alvoTipo.classList.contains("warm-up")){
+        alvoEvento.textContent = "W"
+        alvoEvento.style.color = "#ffc124"
+      }else if(alvoTipo.classList.contains("normal")){
+        alvoEvento.textContent = "N"
+        alvoEvento.style.color = "#FFFFFF"
+      }else if(alvoTipo.classList.contains("failure")){
+        alvoEvento.textContent = "F"
+        alvoEvento.style.color = "#ff0808"
+      }else if(alvoTipo.classList.contains("drop")){
+        alvoEvento.textContent = "D"
+        alvoEvento.style.color = "#0095ff"
+      }
+      alvoEvento = null
+    })
+  }
   });
